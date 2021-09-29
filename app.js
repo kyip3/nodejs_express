b292403587c8810659a17aeff6c07854fe7c51cb
Yip Ka Yan <yipkayan3@gmail.com>
2021-09-29 22:07:13 +0800

    installed expressed js
    npm install express --save
    
    added middleware testing!

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-//if is referring to a file in local use ./http
-const http = require('http'); //require is reserved keywword
-const routes = require('./routes'); //node will look for this file, and look for module export
+const http = require('http'); 
+const express = require('express');
 
-const server = http.createServer(routes);
+const app = express();
 
+app.use((req,res,next)=>{
+    console.log('in middleware');
+    next(); // allow request to continue to the next middleware below
+});
+
+app.use((req,res,next)=>{
+    console.log('another middleware');
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
