38c65004c40b0372bd5fc472f163426858d72bef
Yip Ka Yan <yipkayan3@gmail.com>
2021-09-29 22:09:53 +0800

    sending response to app
    express take care of header for you.
    can overwrite the header if needed.
    
    run: npm start

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use((req,res,next)=>{
 
 app.use((req,res,next)=>{
     console.log('another middleware');
+    res.send('<h1>hello from express!</h1>');
 });
 
 const server = http.createServer(app);
