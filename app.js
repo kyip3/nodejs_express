338435eaee8c8b15c57912265f9ebc815e7eed9c
Yip Ka Yan <yipkayan3@gmail.com>
2021-09-30 21:20:42 +0800

    added 404
    show 404 when access invalid routes

--- app.js
+++ app.js
@@ -13,5 +13,8 @@ app.use(express.urlencoded({
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use('/',(req,res,next)=>{
+    res.status(404).send('<h1>Page not found.</h1>');
+});
 
 app.listen(3000);
